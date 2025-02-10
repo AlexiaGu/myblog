6727aaac4087467f73550ff9150b286ec2a54891
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-02-10 17:43:58 +0100

    quest8 : Relations Many-to-Many complexes (avec attributs)

---
+++ src/main/java/org/wildcodeschool/myblog/dto/AuthorDTO.java
@@ -0,0 +1,43 @@
+package org.wildcodeschool.myblog.dto;
+
+import java.util.List;
+
+public class AuthorDTO {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private List<ArticleDTO> articles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<ArticleDTO> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<ArticleDTO> articles) {
+        this.articles = articles;
+    }
+}
+
